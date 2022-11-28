1fc029180df1047a902c2002bfa14963459e61ff
Jone Wang <i@jone.wang>
2022-11-28 10:00:26 +0800

    Update.

--- Sources/Coral/Models/WebApiServerCredential.swift
+++ Sources/Coral/Models/WebApiServerCredential.swift
@@ -4,7 +4,7 @@ public struct WebApiServerCredential: Codable {
     public let accessToken: String
     public let expiresIn: Int64
 
-    init(accessToken: String, expiresIn: Int64) {
+    public init(accessToken: String, expiresIn: Int64) {
         self.accessToken = accessToken
         self.expiresIn = expiresIn
     }
