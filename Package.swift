c51f680322ae88e62f4b306e550ba92bff1e18ec
Xander Deng <dengxiang2010@gmail.com>
2022-12-04 17:43:19 +0800

    fix name collision

--- Package.swift
+++ Package.swift
@@ -12,23 +12,16 @@ let package = Package(
         .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Coral",
-            targets: ["Coral"]),
-        .executable(name: "coral", targets: ["CoralCLI"]),
+        .library(name: "Coral", targets: ["Coral"]),
+        .executable(name: "coralcli", targets: ["CoralCLI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"3.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/imink-app/InkMoya.git", branch: "main")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Coral",
             dependencies: [
@@ -37,12 +30,14 @@ let package = Package(
                 .product(name: "InkMoya", package: "InkMoya"),
             ],
             resources: [.copy("API/SampleData")]),
+        
         .executableTarget(
             name: "CoralCLI",
             dependencies: [
                 "Coral",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]),
+        
         .testTarget(
             name: "CoralTests",
             dependencies: ["Coral"]),
